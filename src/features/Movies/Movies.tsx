737b50d6675e3140ed9c2f52ce76e6ba469ca15a
Volodymyr Nerovnia <nerv@i.ua>
2024-04-05 14:08:26 +0300

    Update Movies component

--- src/features/Movies/Movies.tsx
+++ src/features/Movies/Movies.tsx
@@ -1,3 +1,13 @@
-export function Movies() {
-  return <div>Movies</div>
+import { Movie } from "../../reducers/movies"
+
+interface MoviesProps {
+  movies:  Movie[]
+}
+
+export function Movies({movies}: MoviesProps) {
+  return (
+    <div>
+      <p>Movies</p>
+    </div>
+  )
 }
